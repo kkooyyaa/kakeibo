fcb82fbeb7918f212f47296e97180e51e3ba8ff7
江口航野 <koya@koya-eguchi.local>
2023-08-26 19:02:41 +0900

    家計簿

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/',      // パスが "/" のときの設定
+    name: 'Home',   // このルートに "Home" という名前をつける
+    component: Home // router-view の中に Home コンポーネントを表示する
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   }
 ]
 
