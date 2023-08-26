b4860ec01d2ea9c786153c7511a362b4edd68479
江口航野 <koya@koya-eguchi.local>
2023-08-26 19:05:36 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  publicPath: isProd ? '/kakeibo' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
